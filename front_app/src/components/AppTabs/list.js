22c66ab400c90ae355a06cd5e4fd90980486e558
comet <sperocometa@naver.com>
2021-08-15 18:14:18 +0900

    Update tour list : dropdown bar

--- front_app/src/components/AppTabs/list.js
+++ front_app/src/components/AppTabs/list.js
@@ -1,5 +1,5 @@
 import {useRoute} from '@react-navigation/native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Text,
   View,
@@ -9,13 +9,16 @@ import {
   Dimensions,
   ScrollView,
 } from 'react-native';
-import {lgreen} from '../../assets/color';
-import {bold} from '../../assets/font';
+import {green, lgreen, lighter, ogreen, ygreen} from '../../assets/color';
+import {bold, plane} from '../../assets/font';
 
 import style from '../../assets/style';
 import ListItem from '../subTabs/ListItem';
 // <TouchableOpacity
 //         onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}></TouchableOpacity>
+
+import {Chip} from 'react-native-paper';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const {width, height} = Dimensions.get('window');
 
@@ -36,6 +39,16 @@ const data = [
   },
 ];
 function List({navigation, route}) {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: '농어촌 체험', value: '농어촌'},
+    {label: '자연생태 체험', value: '자연생태'},
+    {label: '승마체험', value: '승마'},
+    {label: '갯벌체험', value: '갯벌'},
+    {label: '수확체험', value: '수확'},
+  ]);
+
   useEffect(() => {}, []);
   return (
     <View style={styles.container}>
@@ -46,7 +59,71 @@ function List({navigation, route}) {
         />
         <Text style={style.title}> {route.params.loc} </Text>
       </View>
-      <View></View>
+
+      <DropDownPicker
+        placeholder="체험 유형을 선택해주세요"
+        placeholderStyle={{
+          color: 'grey',
+          fontFamily: bold,
+        }}
+        multiple={true}
+        mode="BADGE"
+        searchable={true}
+        translation={{
+          SEARCH_PLACEHOLDER: '원하는 체험 유형을 검색해보세요!',
+        }}
+        searchContainerStyle={{
+          borderBottomColor: '#dfdfdf',
+          backgroundColor: 'white',
+        }}
+        searchTextInputStyle={{
+          fontFamily: plane,
+
+          borderWidth: 0,
+        }}
+        searchPlaceholderTextColor="grey"
+        customItemContainerStyle={{
+          backgroundColor: 'red',
+        }}
+        listItemLabelStyle={{
+          fontSize: 20,
+          marginLeft: 20,
+        }}
+        selectedItemLabelStyle={{
+          fontFamily: bold,
+          color: 'white',
+        }}
+        selectedItemContainerStyle={{
+          backgroundColor: lgreen,
+        }}
+        listItemContainer={{
+          height: 40,
+        }}
+        listMode="MODAL"
+        open={open}
+        showBadgeDot={true}
+        badgeStyle={{
+          padding: 5,
+          backgroundColor: 'white',
+          borderColor: '#E1E1E1',
+          borderWidth: 1,
+        }}
+        badgeTextStyle={{
+          fontFamily: bold,
+          color: 'black',
+        }}
+        badgeDotStyle={{
+          borderRadius: 20,
+        }}
+        badgeDotColors={[green, ogreen, lgreen, ygreen, lighter]}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        dropDownContainerStyle={styles.dropDownContainerStyle}
+        style={styles.dropdown}
+      />
       <ScrollView contentContainerStyle={styles.tour}>
         {data.map((i, index) => (
           <ListItem key={index} info={i} navigation={navigation} />
@@ -75,5 +152,23 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     marginTop: 5,
     marginBottom: 10,
+  },
+  dropdown: {
+    backgroundColor: '#EFEFEF',
+    fontFamily: bold,
+    marginVertical: 10,
+    width: width - 50,
+    left: 50 / 2,
+    borderWidth: 0,
+  },
+  dropDownContainerStyle: {
+    width: width - 50,
+    left: 50 / 2,
+    flexWrap: 'wrap',
+    display: 'flex',
+  },
+  chips: {
+    alignItems: 'flex-start',
+    width: width - 50,
   },
 });
