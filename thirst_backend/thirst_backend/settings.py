aac04da5f76ebceff5d8ae35801525802cc1f1f4
comet <sperocometa@naver.com>
2021-08-18 16:17:08 +0900

    Modif setting for cloud connection

--- thirst_backend/thirst_backend/settings.py
+++ thirst_backend/thirst_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9_s(7z9lvd(#w&3csl)9hf@yq0@k+f9bo+2$xchbqi!4ta83d+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
